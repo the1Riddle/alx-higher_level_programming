e16bec9aa8f2016d2d4fa1e2fd18c53039b2e3f0
elvismalsa <elvisotienomboya@gmail.com>
2023-08-07 08:31:59 -0700

    copy cut and paste

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"First 3 letters: {word[:3]}")
+print(f"Last 2 letters: {word[-2:]}")
+print(f"Middle word: {word[1:-1]}")
