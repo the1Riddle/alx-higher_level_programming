3c354da2f34ffd53bdfe3e753a965e442d724a71
elvismalsa <elvisotienomboya@gmail.com>
2023-08-08 09:49:28 -0700

    prints the l_digit and check if >5, 0, <6 && !0

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+l_digit = abs(number) % 10
+if number < 0:
+    l_digit = -l_digit
+print(f"Last digit of {number} is {l_digit} and is ", end = "")
+if l_digit > 5:
+    print("greater than 5")
+elif l_digit == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
