bfea548d70014d342394789c1669890bef747db7
elvismalsa <elvisotienomboya@gmail.com>
2023-08-14 08:55:18 -0700

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,8 +3,7 @@
     without modifying the original lis
 """""
 def new_in_list(my_list, idx, element):
-
-    if (idx < 0) | (idx > (len(my_list)-1)):
+    if ((idx < 0) | (idx > (len(my_list)-1))):
         return (my_list)
 
     copy = [x for x in my_list]
