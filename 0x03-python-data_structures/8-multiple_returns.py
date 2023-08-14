950ac36c4613951897cedcaf3592e04337770fd0
elvismalsa <elvisotienomboya@gmail.com>
2023-08-14 05:20:11 -0700

    returns a tuple with the length of a string and its first character

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""""
+    requirement :
+    If the sentence is empty, the first character should be equal to None
+"""""
+def multiple_returns(sentence):
+    if sentence == '':
+        return (0, None)
+    return (len(sentence), sentence[0])
