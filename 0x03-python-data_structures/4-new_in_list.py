bfc0386839c7df2e3c0e6f17150e2eb51de96a72
elvismalsa <elvisotienomboya@gmail.com>
2023-08-14 10:28:48 -0700

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""""a function that replaces an element in a list at a specific position
-    without modifying the original lis
-"""""
 def new_in_list(my_list, idx, element):
+    """
+        a function that replaces an element in a list at a specific position
+        without modifying the original lis
+    """
     if ((idx < 0) | (idx > (len(my_list)-1))):
         return (my_list)
 
