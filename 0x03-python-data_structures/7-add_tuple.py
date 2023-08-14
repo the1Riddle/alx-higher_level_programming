43f6c9397e2d84d028fa69333ba2e24ca21812b1
elvismalsa <elvisotienomboya@gmail.com>
2023-08-14 10:38:24 -0700

    a function that adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     """"
-      the function will return 2 tuples where:
-      the 1st element should be the addition of the 1st element of each argument
-      then the 2nd should be the addition of the second element of each argument
-      If a tuple is smaller than 2, use the value 0 for each missing integer
-      If a tuple is bigger than 2, use only the first 2 integers
+    the function will return 2 tuples where:
+    the 1st element should be the addition of the 1st element of each argument
+    then the 2nd should be the addition of the second element of each argument
+    If a tuple is smaller than 2, use the value 0 for each missing integer
+    If a tuple is bigger than 2, use only the first 2 integers
     """""
     len_a = len(tuple_a)
     len_b = len(tuple_b)
