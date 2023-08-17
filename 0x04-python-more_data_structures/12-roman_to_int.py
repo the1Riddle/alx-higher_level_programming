d9359de3dd44a3d8c6042aff28c1b781e89c9b24
elvismalsa <elvisotienomboya@gmail.com>
2023-08-17 03:47:35 -0700

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,8 +6,7 @@ def roman_to_int(roman_string):
     romaN = 0
     for j in range(len(roman_string)):
         if j > 0 and romanVls[roman_string[j]] > romanVls[roman_string[j - 1]]:
-            romaN += romanVls[roman_string[j]] - 2 *
-            romanVls[roman_string[j - 1]]
+            romaN += romanVls[roman_string[j]] - 2 * romanVls[roman_string[j - 1]]
         else:
             romaN += romanVls[roman_string[j]]
     return (romaN)
