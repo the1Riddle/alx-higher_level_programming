964735bd8da0fba4ee928f5cb20911fcff703736
elvismalsa <elvisotienomboya@gmail.com>
2023-08-29 11:56:13 -0700

    Printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" a class Square that defines a square """
+
+
 class Square:
-    """ a class Square that defines a square """
+    """ the class body """
     def __init__(self, size):
         """
             Square contructor
