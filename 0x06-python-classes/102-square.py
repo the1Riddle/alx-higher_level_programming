fe4661a04475ab7d7c1c91ba30f0fdb9dcc3ea26
elvismalsa <elvisotienomboya@gmail.com>
2023-08-29 13:09:07 -0700

    Compare 2 squares, but am doing wonders 😂😂 or is it rumous i dont know 😂😂

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""  class Square that defines a square """
+
+
+class Square:
+    """the class body"""
+
+    def __init__(self, size=0):
+        """
+            square contructor 
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+            the size of the length
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """ am just tryng out this fomulae"""
+        return self.__size * self.__size
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
