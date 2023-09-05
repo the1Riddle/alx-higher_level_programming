3b9c3ca6c59028fbaa32b908d5b876a358483969
elvismalsa <elvisotienomboya@gmail.com>
2023-09-05 05:46:21 -0700

    Copy a list object

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,11 @@
+#!/urs/bin/python3
+"""a func that return a copy"""
+
+
+def copy_list(l):
+    """
+        The input list can contain any type of objects
+        Your file should be maximum 3-line long (no documentation needed)
+        You are not allowed to import any module
+    """
+    return (l[:])
