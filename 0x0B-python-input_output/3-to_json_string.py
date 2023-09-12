ca62dcb983ff3f4e5373f11dc066d26134d158e5
elvismalsa <elvisotienomboya@gmail.com>
2023-09-12 03:39:40 -0700

    json representation of an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    a function that returns the JSON representation of an object (string)
+"""
+
+
+def to_json_string(my_obj):
+    """the body"""
+    return (json.dumps(my_obj))
