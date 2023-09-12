68b850685379c2c917f810b2df74c48456bb625e
elvismalsa <elvisotienomboya@gmail.com>
2023-09-12 05:35:55 -0700

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
-            __import__('6-load_from_json_file').load_from_json_file
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:
         thelist = load_from_json_file("add_item.json")
