82776ccae623248ca82db31320ab97bc1cd42022
the1Riddle <elvisotienomboya@gmail.com>
2023-10-17 19:04:52 +0300

    group then order number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,5 @@
 -- a script that lists the number of records with the same score in the table
-SELECT score, COUNT(*) AS number FROM select_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
