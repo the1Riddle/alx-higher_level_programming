ffa64a0396365616658789853d704a465e994c8c
the1Riddle <elvisotienomboya@gmail.com>
2023-10-17 17:53:25 +0300

    full creation of the table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,15 @@
+-- a script that creates a table in the Db and adds myltiple rows
+CREATE TABLE IF NOT EXISTS second_table
+(
+	id INT
+	name VARCHAR(256)
+	score INT
+);
+INSERT INTO second_table (id, name, score)
+VALUES
+(
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8),
+);
