e9c2236b2d631d51eb2ec0c76610efa918686bf6
the1Riddle <elvisotienomboya@gmail.com>
2023-10-17 18:11:23 +0300

    updates

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- a script that creates a table in the Db and adds myltiple rows
 CREATE TABLE IF NOT EXISTS second_table
 (
-	id INT
-	name VARCHAR(256)
+	id INT,
+	name VARCHAR(256),
 	score INT
 );
 INSERT INTO second_table (id, name, score)
