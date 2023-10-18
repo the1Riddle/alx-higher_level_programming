c2cf7d30daafae6f39d33df74621bccb0429c861
the1Riddle <elvisotienomboya@gmail.com>
2023-10-18 13:49:11 -0700

    without genre linked

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- a script that lists all shows contained without genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.show_id is NULL
+ORDER BY tv_shows.title;
