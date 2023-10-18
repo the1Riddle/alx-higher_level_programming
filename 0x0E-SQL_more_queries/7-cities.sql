e3e8f66ec41153575acf2e0707f55f03d9b535e1
the1Riddle <elvisotienomboya@gmail.com>
2023-10-18 11:39:00 -0700

    creates a table with a foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- a script that creates a DB and a table on the server
+CREATE DATABASE IF NOTY EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities;
+(
+	id INT AUTO_INCRIMENT,
+	PRIMARY KEY (id),
+	state_id INT NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id)
+	name VARCHAR(256) NOT NULL
+);
