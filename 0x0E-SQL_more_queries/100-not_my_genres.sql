968f0bf189aa04f198f253e5ea7e97c763328313
the1Riddle <elvisotienomboya@gmail.com>
2023-10-18 13:54:54 -0700

    lists from unlinked genre

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,10 @@
+-- a script that uses a Db to list all genres not linked to the show Dexter
+SELECT tv_genres.name FROM tv_genres
+WHERE tv_genres.name NOT IN
+(
+	SELECT tv_genres.name FROM tv_genres
+	JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+	JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+	WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name;
