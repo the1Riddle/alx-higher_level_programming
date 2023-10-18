ee1069fe6c87d3266e43055f6b97534172263240
the1Riddle <elvisotienomboya@gmail.com>
2023-10-18 12:11:23 -0700

    joins the ids

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- a script that lists some contents of a Db
 SELECT cities.id, cities.name, states.name
-FROM cities JOIN states ON cities.states_id = states.id;
+FROM cities JOIN states ON cities.state_id = states.id;
