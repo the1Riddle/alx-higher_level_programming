a4bc1d054788c214a420b11e023f37f52413e7ce
the1Riddle <elvisotienomboya@gmail.com>
2023-10-18 09:42:28 -0700

    a table with a primary key id

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- a script that creates a database and a table in the server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 -- now creating the table
 CREATE TABLE IF NOT EXISTS states
 (
