415842657b4ed9834ec0bf99cc29973fd995fedb
the1Riddle <elvisotienomboya@gmail.com>
2023-10-18 12:17:17 -0700

    Genre ID by shows tv shows

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,4 @@
+-- a script that lists all shows contained that have at least one genre linked
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_show_genres JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
