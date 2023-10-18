47a80d059a4a1fd2279eb598f83ab24d9287af7d
the1Riddle <elvisotienomboya@gmail.com>
2023-10-18 09:03:19 -0700

    check

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
 -- a script that creates a sever for a user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
+
+-- grant the user acces to the root
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
