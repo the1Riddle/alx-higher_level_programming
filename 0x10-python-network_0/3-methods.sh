7d86f976be7b8ef52f045f77b3a8578c5fccb473
the1Riddle <elvisotienomboya@gmail.com>
2023-12-05 11:16:29 +0300

    make it exactly 3 lines long (wc -l file should print 3)

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# a script that takes in a URL and displays,
-# all HTTP methods the server will accept
+# a script that takes in a URL and displays, all HTTP methods the server will accept
 curl -sI "$1" | grep "Allow: " | cut -d' ' -f 2-
